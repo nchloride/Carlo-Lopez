d76779c59424871dc44310824ef065a482c6401e
nchloride <clankz39@gmail.com>
2021-02-25 19:16:46 +0800

    added: date on home intro

--- frontend/src/Routes/Home/HomeIntroduction.js
+++ frontend/src/Routes/Home/HomeIntroduction.js
@@ -5,27 +5,30 @@ import chorvald from "../../Components/pictures/ChorVald.jpg";
 import donttouchme from "./soundFX/donttouchme.m4a";
 import iloveyou from "./soundFX/iloveyou.m4a";
 
-
-
 const HomeIntroduction = () => {
-    const soundClip = [,donttouchme,iloveyou];
+    const soundClip = ['',donttouchme,iloveyou];
     const [randomNumber,setRandomNumber] = useState(1);
     const [play] = useSound(soundClip[randomNumber]);
 
-    const tagLines = ["Hello there! You are in my website","that means","I already know your personal information","Just kidding I'm dumb","GOOD DAY SIR!"];
+    const tagLines = ["Please!","do not ","hover your cursor","on my picture"];
     const onMouseEnter = () =>{
         play();
         setRandomNumber(Math.ceil(Math.random() *( soundClip.length)-1));
     }
 
     return (
-        <section className="home__intro">
-            <motion.img onMouseEnter={onMouseEnter} animate={{rotate:360}} src={chorvald}></motion.img>
-            <div className="tagLines">
-                {tagLines.map((tagLine,index)=><TagLineContainer tagLine={tagLine} delay={index} key={index}/>)}
-            </div>
-           
-        </section>
+        <>
+            <section className="home__intro">
+                    <div className="intro__picture">
+                        <motion.img onMouseEnter={onMouseEnter} animate={{rotate:360}} src={chorvald}></motion.img>
+                        <h1>Noel Carlo Lopez</h1>
+                        <h2>1999-????</h2>
+                    </div>
+                    <div className="tagLines">
+                        {tagLines.map((tagLine,index)=><TagLineContainer tagLine={tagLine} delay={index} key={index}/>)}
+                    </div>
+            </section>
+        </>
     )
 }
 const TagLineContainer = ({tagLine,delay})=> {
